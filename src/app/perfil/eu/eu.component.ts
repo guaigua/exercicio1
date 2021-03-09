9efaffb1e191bf9b5bf570fdd5145296fe03fd37
guaigua <gerguaigua@gmail.com>
2021-03-09 03:17:44 -0300

    Manipulaçao Dinâmica

--- src/app/perfil/eu/eu.component.ts
+++ src/app/perfil/eu/eu.component.ts
@@ -10,7 +10,10 @@ import { DataService } from '../../services/data.service';
 export class EuComponent implements OnInit {
 
   url: string = 'https://github.com/guaigua/';
-  urlImage: string = 'https://avatars.githubusercontent.com/u/4908471?s=460&u=74ea969ebb8a126662207b0456a819fba1a801f5&v=4'
+  urlImage: string = 'https://avatars.githubusercontent.com/u/4908471?s=460&u=74ea969ebb8a126662207b0456a819fba1a801f5&v=4';
+  meu_nome: string = 'German Guaigua';
+  meu_personalidade: string ='Desenvolvedor Full Stack. Adoro programação e Treekking. Usuário Linux desde 2005, Software Livre, Amante de código aberto. Usuário Debian, Linux';
+
   productos: any;
 
   constructor(productos: DataService) {
