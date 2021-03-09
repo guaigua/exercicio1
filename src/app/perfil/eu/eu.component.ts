ac026964605adfbee64cc8d94304ee02ce414f38
guaigua <gerguaigua@gmail.com>
2021-03-09 03:08:26 -0300

    Interpolação

--- src/app/perfil/eu/eu.component.ts
+++ src/app/perfil/eu/eu.component.ts
@@ -9,7 +9,8 @@ import { DataService } from '../../services/data.service';
 })
 export class EuComponent implements OnInit {
 
-  
+  url: string = 'https://github.com/guaigua/';
+  urlImage: string = 'https://avatars.githubusercontent.com/u/4908471?s=460&u=74ea969ebb8a126662207b0456a819fba1a801f5&v=4'
   productos: any;
 
   constructor(productos: DataService) {
