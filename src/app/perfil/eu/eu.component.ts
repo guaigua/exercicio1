cb4769240341f858bd7fb4656fd30b05f1cf1e23
guaigua <gerguaigua@gmail.com>
2021-03-11 18:45:01 -0300

    Removendo Bagunça

--- src/app/perfil/eu/eu.component.ts
+++ src/app/perfil/eu/eu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ɵangular_packages_platform_browser_platform_browser_d } from '@angular/platform-browser';
 import { DataService } from '../../services/data.service';
 
 
@@ -13,8 +14,18 @@ export class EuComponent implements OnInit {
   urlImage: string = 'https://avatars.githubusercontent.com/u/4908471?s=460&u=74ea969ebb8a126662207b0456a819fba1a801f5&v=4';
   meu_nome: string = 'German Guaigua';
   meu_personalidade: string ='Desenvolvedor Full Stack. Adoro programação e Treekking. Usuário Linux desde 2005, Software Livre, Amante de código aberto. Usuário Debian, Linux';
+  campo_texto: string = '';
+  valor_salvo: string = '';
 
   productos: any;
+  passwordTrue: boolean = true;
+  inputType: string = 'Password';
+ 
+  botaoClicado() {
+      alert("Obrigado");
+  }
+
+
 
   constructor(productos: DataService) {
    
@@ -23,5 +34,6 @@ export class EuComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 
 }
